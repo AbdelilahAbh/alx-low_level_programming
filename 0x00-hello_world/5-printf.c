dc56a27b4ee5efd4870b630d8cd87dd7c5c9648b
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-03-16 10:05:30 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Print special message
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
