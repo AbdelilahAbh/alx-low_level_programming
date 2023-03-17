411fafddcea2a71a5a46bd1860f3faffd4fdf3ee
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-03-17 09:42:17 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Print special message
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
+}
