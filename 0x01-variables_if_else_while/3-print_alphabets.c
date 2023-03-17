ea61b7a95487ae7b127878d2276672417315252b
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-03-17 08:49:02 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - Print special message
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	char x = 'A';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	while (x <= 'Z')
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
