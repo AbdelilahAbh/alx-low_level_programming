a3d8ce4fbdc22a510e3026522e415c61d64947e6
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-03-21 10:16:07 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isalpha - check for alphabetic
+ *
+ *@c: character in ASCII
+ *
+ *Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+	_putchar('\n');
+}
