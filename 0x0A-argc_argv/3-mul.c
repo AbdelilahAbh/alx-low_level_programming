fb161cafa3930d17b7f2baaf2c5e499481e9228c
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-09 08:09:08 -0700

    task 3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the name of file c
+ *
+ * @argc: number of chose in command line
+ * @argv: array of string contain element entre in command line
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc < 2)
+		printf("Error\n");
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", mul);
+	}
+	return (1);
+}
