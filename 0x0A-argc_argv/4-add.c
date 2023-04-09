62e86943a130cc36501d156f6ca386014434f1c0
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-09 09:13:08 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * check_num - check inputs are digit
+ *
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	/*Declaring variables*/
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if str there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+
+/**
+ * main - print the name of file c
+ *
+ * @argc: number of chose in command line
+ * @argv: array of string contain element entre in command line
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 1; i < argc; i++)
+	{
+		if (check_num(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
