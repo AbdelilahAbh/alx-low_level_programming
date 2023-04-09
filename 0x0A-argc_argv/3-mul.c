a2f839d466e7da0d8deca93764b7f3da2564f9b4
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-09 08:43:58 -0700

    fix task 3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,24 +2,23 @@
 #include <stdlib.h>
 
 /**
- * main - print the name of file c
+ * main - multiple of two number
  *
  * @argc: number of chose in command line
  * @argv: array of string contain element entre in command line
  *
- * Return: Always 0.
+ * Return: integer.
  */
-
 int main(int argc, char *argv[])
 {
 	int mul;
 
 	if (argc < 2 || argc > 3)
-		printf("Error\n");
-	else
 	{
-		mul = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", mul);
+		printf("Error\n");
+		return (1);
 	}
-	return (1);
+	mul = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", mul);
+	return (0);
 }
