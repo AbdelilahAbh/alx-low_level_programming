ececc703e3e2ac39293498e1427d5f5e0d9d2e52
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-11 11:09:37 -0700

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * alloc_grid - array of 2 dimensional
+ *
+ * @width: first string
+ * @height: seconde string
+ *
+ * Return: integer.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **arr;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+	arr = malloc(sizeof(int *) * height);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+
+	return (arr);
+}
