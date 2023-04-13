0d1a730ac32a62b61fa262b766c3a51b7f043adb
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-13 14:46:20 -0700

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * mset - fills memory with a constant byte
+ * @s: memory area
+ * @b: char
+ * @n: number
+ *
+ * Return: pointer to the memory area s
+ */
+char *mset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ *
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	mset(ptr, 0, nmemb * size);
+	return (ptr);
+}
