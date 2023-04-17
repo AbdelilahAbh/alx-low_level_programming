fb91a1920b7cd63dd5ea9ff53c74cfe022d09bfd
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-17 05:02:40 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initialize struct dog
+ *
+ * @d: structure name
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner dog
+ *
+ * return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner =  owner;
+}
