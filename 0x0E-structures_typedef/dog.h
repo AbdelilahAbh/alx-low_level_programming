84f13983801e1857656c67ff7cedae75f83f8096
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-17 04:27:46 -0700

    task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @owner: owner of dog
+ * @age: age of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+
+#endif
