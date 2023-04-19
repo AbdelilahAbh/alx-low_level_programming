3c744b8b86c52ac40c26be30117123bba6d3dffe
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-19 09:39:35 -0700

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - print a name
+ *
+ * @name: name printed
+ * @f: pointer to functio
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
