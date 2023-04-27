5f317a12669bd8d2ec72d007bfdd94d657cdb9c7
AbdelilahAitbaha <aitbaha.abdelilah2000@gmail.com>
2023-04-27 09:31:18 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - print elements of list_t
+ *
+ * @h: element list_t
+ *
+ * Return: number of node.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+
+		}
+		h = h->next;
+		s++;
+	}
+
+	return (s);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,23 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+/**
+ * struct list_s - create structures
+ * @str: string of structures
+ * @len: len of str
+ * @next: point to the next node
+ * description: singly linked list node structure
+ */
+
+struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+};
+typedef struct list_s list_t;
+
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
